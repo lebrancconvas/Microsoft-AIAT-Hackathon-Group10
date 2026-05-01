c84e54806b8a7239f301bf0e127ac4a1aaa40f5b
lebrancconvas <lebrancconvas@gmail.com>
2026-05-02 02:37:35 +0700

    ✨ feat(api): update post method for the data that come from the front-end.

--- api/main.py
+++ api/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel   
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# class ImageForm(BaseModel):
+#   body: File
 
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
+
+@app.post("/api/predict")
+async def predict(file: UploadFile = File(...)):
+  image_bytes = await file.read()
+  return {"data": file}
